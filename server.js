5223c15f18fa89d312ef6c512d751cf6d4bab05e
pedroluissouza028 <pedroluissouza028@gmail.com>
2026-06-17 20:57:13 -0300

    Adicionar OAuth Santander

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const axios = require("axios");
+const https = require("https");
+const fs = require("fs");
 require("dotenv").config();
 
 const app = express();
@@ -8,6 +11,37 @@ app.get("/", (req, res) => {
   res.json({
     status: "Santander Proxy Online"
   });
+});
+
+app.get("/token", async (req, res) => {
+  try {
+    const pfx = fs.readFileSync("./open.pfx");
+
+    const agent = new https.Agent({
+      pfx,
+      passphrase: process.env.SANTANDER_CERT_PASSPHRASE
+    });
+
+    const response = await axios.post(
+      "https://trust-pix.santander.com.br/oauth/token?grant_type=client_credentials",
+      {},
+      {
+        httpsAgent: agent,
+        auth: {
+          username: process.env.SANTANDER_CLIENT_ID,
+          password: process.env.SANTANDER_CLIENT_SECRET
+        }
+      }
+    );
+
+    res.json(response.data);
+  } catch (err) {
+    console.error(err.response?.data || err.message);
+
+    res.status(500).json({
+      erro: err.response?.data || err.message
+    });
+  }
 });
 
 const PORT = process.env.PORT || 3000;
